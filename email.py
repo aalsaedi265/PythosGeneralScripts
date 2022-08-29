a56eec35846afcbda5996a35c2dbf3ced8be71a6
Ahmed Alsaedi <aalsaedi265@gmail.com>
2022-08-29 13:02:43 -0500

    quit email and login

--- email.py
+++ email.py
@@ -4,3 +4,8 @@ smtObj =smtplib.SMTP('smtp.gmail.com', 587)
 smtObj.ehlo()  #connects the serves snder and recicer
 smtObj.starttls()
 smtObj.login() #username and login
+smtObj.sendmail()
+#email sent too, Email sent from, Subject: items. \n the body
+
+
+smtObj.quit() #end the connection
